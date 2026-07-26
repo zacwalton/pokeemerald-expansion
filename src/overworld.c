9eb100735f248e7bab6090082dd2b453d559356c
zacwalton <51077283+zacwalton@users.noreply.github.com>
2026-07-26 22:29:13 +0100

    Revert "Perma Night DNS blend for caves initial"
    This reverts commit 5d348c5ff3e4f4389834ca70d608ffee55f272cc.

--- src/overworld.c
+++ src/overworld.c
@@ -1665,8 +1665,7 @@ bool32 MapHasNaturalLight(u8 mapType)
          && (mapType == MAP_TYPE_TOWN
           || mapType == MAP_TYPE_CITY
           || mapType == MAP_TYPE_ROUTE
-          || mapType == MAP_TYPE_OCEAN_ROUTE
-          || mapType == MAP_TYPE_UNDERGROUND));
+          || mapType == MAP_TYPE_OCEAN_ROUTE));
 }
 
 bool32 CurrentMapHasShadows(void)
@@ -1720,28 +1719,15 @@ void UpdatePalettesWithTime(u32 palettes)
     palettes &= PALETTES_MAP | PALETTES_OBJECTS; // Don't blend UI pals
     if (!palettes)
         return;
-    if (gMapHeader.mapType == MAP_TYPE_UNDERGROUND)
-    {
-        TimeMixPalettes(palettes, gPlttBufferUnfaded, gPlttBufferFaded, (struct BlendSettings *)&gTimeOfDayBlend[TIME_NIGHT], (struct BlendSettings *)&gTimeOfDayBlend[TIME_NIGHT], 256);
-        return;
-    }
-        TimeMixPalettes(palettes, gPlttBufferUnfaded, gPlttBufferFaded, &gTimeBlend.startBlend, &gTimeBlend.endBlend, gTimeBlend.weight);
+    TimeMixPalettes(palettes, gPlttBufferUnfaded, gPlttBufferFaded, &gTimeBlend.startBlend, &gTimeBlend.endBlend, gTimeBlend.weight);
 }
 
 u8 UpdateSpritePaletteWithTime(u8 paletteNum)
 {
-    if (!IS_BLEND_IMMUNE_TAG(GetSpritePaletteTagByPaletteNum(paletteNum)))
-    {
-        if (gMapHeader.mapType == MAP_TYPE_UNDERGROUND)
-        {
-            TimeMixPalettes(1, &gPlttBufferUnfaded[OBJ_PLTT_ID(paletteNum)], &gPlttBufferFaded[OBJ_PLTT_ID(paletteNum)], (struct BlendSettings *)&gTimeOfDayBlend[TIME_NIGHT], (struct BlendSettings *)&gTimeOfDayBlend[TIME_NIGHT], 256);
-        }
-        else if (MapHasNaturalLight(gMapHeader.mapType))
-        {
-            TimeMixPalettes(1, &gPlttBufferUnfaded[OBJ_PLTT_ID(paletteNum)], &gPlttBufferFaded[OBJ_PLTT_ID(paletteNum)], &gTimeBlend.startBlend, &gTimeBlend.endBlend, gTimeBlend.weight);
-        }   
-    }
-        return paletteNum;
+    if (MapHasNaturalLight(gMapHeader.mapType)
+     && !IS_BLEND_IMMUNE_TAG(GetSpritePaletteTagByPaletteNum(paletteNum)))
+        TimeMixPalettes(1, &gPlttBufferUnfaded[OBJ_PLTT_ID(paletteNum)], &gPlttBufferFaded[OBJ_PLTT_ID(paletteNum)], &gTimeBlend.startBlend, &gTimeBlend.endBlend, gTimeBlend.weight);
+    return paletteNum;
 }
 
 static void OverworldBasic(void)
@@ -1782,9 +1768,6 @@ void CB2_OverworldBasic(void)
 
 void CB2_Overworld(void)
 {
-    if (gMapHeader.mapType == MAP_TYPE_UNDERGROUND)
-        UpdatePalettesWithTime(PALETTES_ALL);
-        
     bool32 fading = (gPaletteFade.active != 0);
     if (fading)
         SetVBlankCallback(NULL);
